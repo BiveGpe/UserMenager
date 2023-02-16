e10f6b78cefb9c4b1869865d1f4b930f7556fd00
BiveGpe <wiktorrapacz@gmail.com>
2023-02-17 00:30:38 +0100

    ValueObject Category

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Infrastructure\Common\ValueObject\Category;
 use Slim\Http\Response;
 use Slim\Http\ServerRequest;
 
@@ -11,6 +12,7 @@ class UserController
 {
     public function getUserById(ServerRequest $request, Response $response): Response
     {
+        $request->withAttribute(Category::USER, new Category('User'));
         $queryParams = $request->getQueryParams();
         $name = $queryParams['id'];
 
