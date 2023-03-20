18edfedfafb11f21689e553743df57d6840899a8
BiveGpe <wiktorrapacz@gmail.com>
2023-03-20 21:37:24 +0100

    cos

---
+++ Tests/Unit/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function testTrim(): void
+    {
+        self::assertSame(5, 5);
+    }
+}
