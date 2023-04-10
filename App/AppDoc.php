8a3d4ebaf6be8d8984cade4b4c5646c1cb361035
BiveGpe <wiktorrapacz@gmail.com>
2023-04-10 22:58:40 +0200

    dokumentacja api

--- App/AppDoc.php
+++ App/AppDoc.php
@@ -4,12 +4,19 @@ namespace App;
 
 use Infrastructure\Common\Interfaces\DocInterface;
 
+//TODO: adds Tags and Components
+
 /**
  * @OA\OpenApi(
  *     openapi="3.1.0",
  *     @OA\Info(
  *         title="UserController",
+ *         description="Communicate with the App by JSON requests and responses, each request must contain Authorization header with token.",
  *         version="1.0.0",
+ *         @OA\Contact(
+ *             name="Wiktor Rapacz",
+ *             email="wiktorrapacz@gmail.com",
+ *         ),
  *     ),
  *     @OA\Server(
  *         url="http://192.168.0.69:2137",
